fbf9535071b10dfec72d220b78e3d372ca88db7a
teamNet <teamnet@outlook.es>
2014-12-03 13:03:14 -0400

    Test FileConverter

---
+++ TddXp/test/tddxp/FileConverterTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tddxp;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author WVasquez
+ */
+public class FileConverterTest {
+    
+    /**
+     * Descargar un  Archivo.
+     */
+    @Test
+    public void descargarArchivo() {
+    }
+    
+    /**
+     * Eliminar un  Archivo.
+     */
+    @Test
+    public void elimiarArchivo() {
+    }
+    
+    /*
+     * Asignar nuevo Archivo a Charla/Curso
+     */
+    @Test
+    public void asignarArchivo(){
+    }
+    
+    /*
+     * Convertir archivo .doc a .pdf
+     */
+    @Test
+    public void convertirDoc() {
+    }
+    
+    /*
+     * Convertir archivo .docx a .pdf
+     */
+    @Test
+    public void convertirDocx() {
+    }
+    
+    /*
+     * Convertir archivo .ppt a .pdf
+     */
+    @Test
+    public void convertirPpt() {
+    }
+    
+    /*
+     * Convertir archivo .pptx a .pdf
+     */
+    @Test
+    public void convertirPptx() {
+    }
+}
